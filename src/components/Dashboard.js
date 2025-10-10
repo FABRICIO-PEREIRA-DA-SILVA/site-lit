a9c6d0b7abc129a1bd4d267c2426714b278d0d5f
FABRICIO-PEREIRA-DA-SILVA <fabriciopereira.tecnico@gmail.com>
2025-10-09 23:42:30 -0300

    Update Dashboard.js

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -409,7 +409,7 @@ function Dashboard({ user }) {
                     amostraColetada: e.target.checked
                   }))
                 }
-                style={{ marginRight: "6px" }}
+                style={{ marginRight: "66px" }}
               />
               Apenas com amostra coletada
             </label>
