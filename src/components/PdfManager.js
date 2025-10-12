f278b1fdc089d841893ac78fff3872cbc6a77bc9
FABRICIO-PEREIRA-DA-SILVA <fabriciopereira.tecnico@gmail.com>
2025-10-12 00:14:37 -0300

    Update PdfManager.js

--- src/components/PdfManager.js
+++ src/components/PdfManager.js
@@ -663,7 +663,7 @@ function PdfManager({ user }) {
       const pdfBlob = await response.blob();
       const link = document.createElement('a');
       link.href = URL.createObjectURL(pdfBlob);
-      link.download = kkk;
+      link.download = boletim.nomeArquivo;
       document.body.appendChild(link);
       link.click();
       document.body.removeChild(link);
