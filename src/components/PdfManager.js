296c0f12147a93354b426bbd083bfda0329d9377
FABRICIO-PEREIRA-DA-SILVA <fabriciopereira.tecnico@gmail.com>
2026-02-02 17:47:44 -0300

    Update PdfManager.js

--- src/components/PdfManager.js
+++ src/components/PdfManager.js
@@ -2125,8 +2125,8 @@ function PdfManager({ user }) {
                           src={labData.assinaturaLaboratorista} 
                           alt="Assinatura" 
                           style={{ 
-                            maxHeight: '60px',      // ⬅️ Altura menor (era 80px)
-                            maxWidth: '250px',      // ⬅️ Largura maior (não tinha)
+                            maxHeight: '20px',      // ⬅️ Altura menor (era 80px)
+                            maxWidth: '2500px',      // ⬅️ Largura maior (não tinha)
                             display: 'block' 
                           }}
                         />
